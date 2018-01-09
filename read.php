04380eb8fd4359626c1ce0ebe950fcd1c7ec9029
Huang-KH <35246297+Huang-KH@users.noreply.github.com>
2018-01-09 11:46:29 +0800

    Update read.php

--- read.php
+++ read.php
@@ -1,5 +1,5 @@
 <?php
-$json_str = "file_get_contents('php://input');//接收Request的Body
+$json_str = file_get_contents('php://input');//接收Request的Body
 $json_obj = json_decode($json_str);//轉json格式
 $myfile=fopen("log.txt","w+") or die("Unable to open file!");//設定一個log.txt用來印訊息
 fwrite($myfile,"\xEF\xBB\xBF",$json_str);//在字串前加入\xEF\xBB\xBF轉成utf8格式
