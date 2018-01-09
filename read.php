0879c04e0a605b03bf3f2f3aa3f3beb5a35575b3
Huang-KH <35246297+Huang-KH@users.noreply.github.com>
2018-01-09 15:00:05 +0800

    Update read.php

--- read.php
+++ read.php
@@ -1,46 +1,9 @@
-//<?php
-//$json_str = file_get_contents('php://input');//接收Request的Body
-//$json_obj = json_decode($json_str);//轉json格式
-//$myfile=fopen("log.txt","w+") or die("Unable to open file!");//設定一個log.txt用來印訊息
-//fwrite($myfile,"\xEF\xBB\xBF".$json_str);//在字串前加入\xEF\xBB\xBF轉成utf8格式
-//fclose($myfile);
-//?>
-
-//<?php
-// $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
-// $json_obj = json_decode($json_str); //轉JSON格式
-// //產生回傳給line server的格式
-// $sender_userid = $json_obj->events[0]->source->userId;
-// $sender_txt = $json_obj->events[0]->message->text;
-// $sender_replyToken = $json_obj->events[0]->replyToken;
-//$response = array (
-//				//"to" => $sender_userid,
-//	 			"replyToken" => $sender_replyToken,
-//				"messages" => array (
-//					array (
-//						"type" => "text",
-//						"text" => "Hello, YOU SAY ".$sender_txt
-//					)
-//				)
-//		);
-// $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
-// fwrite($myfile, "\xEF\xBB\xBF".json_decode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
-// fclose($myfile);
-// //回傳給line server
-// $header[] = "Content-Type: application/json";
-// $header[] = "Authorization: Bearer 6EYmw4KdaVmfZzfyFuTLbiNoSGvereNnvODaypsR62/h0/Sws49gvmBoEzSq7MMdsZontZUXgIwFSt9LXSVNvMunF6Rhor1NAdisXJCgO3RBAk9P81REOpZrlw8g+QJOxkKkUedSzRUnLGi3jo1f7wdB04t89/1O/w1cDnyilFU=";
-// //$ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
-// $ch = curl_init("https://api.line.me/v2/bot/message/reply");                                                                      
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
-// $result = curl_exec($ch);
-// curl_close($ch); 
-//?>*/
 <?php
  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
  $json_obj = json_decode($json_str); //轉JSON格式
+$myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
+ fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ fclose($myfile);
  //產生回傳給line server的格式
  $sender_userid = $json_obj->events[0]->source->userId;
  $sender_txt = $json_obj->events[0]->message->text;
@@ -112,10 +75,37 @@
 				)
 			);
         		break;
+		 case "button":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "template",
+						"altText" => "this is a buttons template",
+						"template" => array (
+							"type" => "buttons",
+							"thumbnailImageUrl" => "https://www.w3schools.com/css/paris.jpg",
+							"title" => "Menu",
+							"text" => "Please select",
+							"actions" => array (
+								array (
+									"type" => "postback",
+									"label" => "Buy",
+									"data" => "action=buy&itemid=123"
+								),
+								array (
+									"type" => "postback",
+                   							"label" => "Add to cart",
+                    							"data" => "action=add&itemid=123"
+								)
+							)
+						)
+					)
+				)
+			);
+        		break;
  }
- $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
- fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
- fclose($myfile);
  //回傳給line server
  $header[] = "Content-Type: application/json";
  $header[] = "Authorization: Bearer 6EYmw4KdaVmfZzfyFuTLbiNoSGvereNnvODaypsR62/h0/Sws49gvmBoEzSq7MMdsZontZUXgIwFSt9LXSVNvMunF6Rhor1NAdisXJCgO3RBAk9P81REOpZrlw8g+QJOxkKkUedSzRUnLGi3jo1f7wdB04t89/1O/w1cDnyilFU=";
@@ -127,4 +117,3 @@
  $result = curl_exec($ch);
  curl_close($ch); 
 ?>
-
